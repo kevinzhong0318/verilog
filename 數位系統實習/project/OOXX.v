ecd5bbd2620e08ee6bfa3fe6a691af451e796265
kevinzhong0318 <116836450+kevinzhong0318@users.noreply.github.com>
2025-11-13 14:50:13 +0800

    刪除不必要的檔案並更新 OOXX 模組以修正參數和輸出格式

--- 數位系統實習/project/OOXX.v
+++ 數位系統實習/project/OOXX.v
@@ -1,17 +1,20 @@
-//16-bit diasplay module
 module OOXX#(
     parameter WIDTH = 16,
-    parameter board_color = 16'hFFFF
+    parameter board_color = 16'hFFFF,
+    parameter X_row1 =4'b1001,
+    parameter X_row2 =4'b0110,//1221
+    parameter O_row1 =4'b0110,
+    parameter O_row2 =4'b1001,//1221
 )(  
-    input clk,reset
-    output [WIDTH-1:0] display_col,
-    output [WIDTH-1:0] display_row,
+    input clk,reset,
+    output reg [WIDTH-1:0] display_col,
+    output reg [WIDTH-1:0] display_row
 );
     reg [3:0] state;
     reg [WIDTH-1:0] col_reg [0:15];
     reg [WIDTH-1:0] row_reg [0:15];
+    integer i,j;
     initial begin
-        integer i;
         for(i = 0; i < 16; i = i + 1) begin
             if(i ==4 || i == 5 || i == 10 || i == 11)
                 col_reg[i] = board_color;
@@ -31,10 +34,10 @@ module OOXX#(
     always @(*) begin
         display_col = 0;
         display_row = 0;
-        for (i = 0; i < 16; i = i + 1) begin
-            if(i == state) begin
-                display_col = col_reg[i];
-                display_row = row_reg[i];
+        for (j = 0; j < 16; j = j + 1) begin
+            if(j == state) begin
+                display_col = col_reg[j];
+                display_row = row_reg[j];
             end
         end
     end
